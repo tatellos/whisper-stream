38f322de163dff7f848650b8ff80ade2f26c287c
Mathias Arens <mathias@arens.ma>
2023-06-04 23:19:54 +0200

    Rewrite all in asyncio. It works, but is not using more than one core - and blocks while waiting for the GPU to finish. This is not a huge problem, as the CPU part is quite fast - but it would be nicer to use multiprocessing.

--- browser/websocket.js
+++ browser/websocket.js
@@ -1,8 +1,11 @@
+// We have to use complete files in the backend. Have to assemble the stream there.
+// This means we periodically have to reset the audio file here in the frontend.
+// I.e. with a fixed interval (kind of a buffer-size {in the backend}) we need to stop and start the media recorder. The backend also has to be told when this happens.
 document.getElementById("start").onclick = () => {
     // Get microphone access
     navigator.mediaDevices.getUserMedia({audio: true})
         .then(stream => {
-            const mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/webm;codecs=opus "});
+            const mediaRecorder = new MediaRecorder(stream, {mimeType: "audio/webm;codecs=opus", bitsPerSecond: 256000});
             mediaRecorder.start(1000); // commit every second
 
             const socket = new WebSocket('ws://localhost:8000');
--- server/main.py
+++ server/main.py
@@ -3,22 +3,65 @@ import os
 
 import websockets
 import whisper
+from pydub import AudioSegment
 
-audio_model = whisper.load_model("tiny")
+streamed_audio = 'audio.ogg'
+if os.path.exists(streamed_audio):
+    os.remove(streamed_audio)
+decompressed_wave = "destination.wav"
+if os.path.exists(decompressed_wave):
+    os.remove(decompressed_wave)
 
-bytesToSave = b""
-filename = 'audio.ogg'
+audio_model = whisper.load_model("medium")
+
+print("READY")
+
 
 async def websocket_handler(websocket, path):
-    async for message in websocket:
-        print(f"Received message: {message}")
-        if len(message) > 5:
-            print("appending to file")
-            with open(filename, 'ab') as file:
-                file.write(message)
+    q = asyncio.Queue()
+    listener_task = asyncio.create_task(listen_for_messages(websocket, q))
+    sender_task = asyncio.create_task(send_messages(websocket, q))
 
-            # result = audio_model.transcribe(filename, language="en", task="transcribe")
-            await websocket.send('result["text"]')
+    print("Starting tasks")
+    done, pending = await asyncio.wait(
+        [listener_task, sender_task],
+        return_when=asyncio.FIRST_COMPLETED,
+    )
+
+    print("cleaning up")
+    for task in pending:
+        task.cancel()
+
+
+async def listen_for_messages(websocket, q):
+    try:
+        async for message in websocket:
+            print(f"Received message: {message}")
+            if len(message) > 5:
+                print("appending x bytes to file", len(message))
+                with open(streamed_audio, 'ab') as file:
+                    file.write(message)
+                await q.put(streamed_audio)
+                print("Triggered Queue")
+    except websockets.exceptions.ConnectionClosed:
+        print("ConnectionClosed")
+
+
+async def send_messages(websocket, q):
+    while True:
+        if q.empty():
+            fname = await q.get()
+        while not q.empty():
+            fname = await q.get()
+        print("Converting audio")
+        AudioSegment.from_file(fname).export(decompressed_wave, format="wav")
+        print("Finished wav audio")
+
+        filesize = os.path.getsize(decompressed_wave)
+        print("Transcribing filesize", filesize)
+        result = audio_model.transcribe(decompressed_wave, language="en", task="transcribe")["text"]
+        print("Sending result", result)
+        await websocket.send(result)
 
 
 # Start websocket server on port 8000
