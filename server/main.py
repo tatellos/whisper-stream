41057050d60073f01434d44f3ac0120695d34bef
Mathias Arens <mathias@arens.ma>
2023-06-17 13:37:28 +0200

    Simplified code a bit - only trigger a new transcription when it's not running (when the queue is empty)

--- server/main.py
+++ server/main.py
@@ -7,23 +7,20 @@ import whisper
 from pydub import AudioSegment
 
 streamed_audio = 'audio.ogg'
-if os.path.exists(streamed_audio):
-    os.remove(streamed_audio)
 decompressed_wave = "destination.wav"
-if os.path.exists(decompressed_wave):
-    os.remove(decompressed_wave)
 
+# Load AI, then report that it's done and ready
 audio_model = whisper.load_model("tiny")
-
 print("READY")
 
 
 async def websocket_handler(websocket, path):
+    clean_up_files()
     q = asyncio.Queue()
     listener_task = asyncio.create_task(listen_for_messages(websocket, q))
     sender_task = asyncio.create_task(send_messages(websocket, q))
 
-    print("Starting tasks")
+    print("Starting tasks", path)
     done, pending = await asyncio.wait(
         [listener_task, sender_task],
         return_when=asyncio.FIRST_COMPLETED,
@@ -37,13 +34,14 @@ async def websocket_handler(websocket, path):
 async def listen_for_messages(websocket, q):
     try:
         async for message in websocket:
-            print(f"Received message: {message[:10]}...")
+            # print(f"Received message: {message[:10]}...")
             if len(message) > 5:
                 print("appending x bytes to file", len(message))
                 with open(streamed_audio, 'ab') as file:
                     file.write(message)
-                await q.put(streamed_audio)
-                print("Triggered Queue")
+                if q.empty():
+                    await q.put(streamed_audio)
+                    print("Triggered Queue")
     except websockets.exceptions.ConnectionClosed:
         print("ConnectionClosed")
 
@@ -51,16 +49,13 @@ async def listen_for_messages(websocket, q):
 async def send_messages(websocket, q):
     start_time = 0
     while True:
-        if q.empty():
-            fname = await q.get()
-        while not q.empty():
-            fname = await q.get()
-        print("Converting audio")
+        fname = await q.get()
+        # print("Converting audio")
         all_sound = AudioSegment.from_file(fname, format="webm", codec="opus")
-        print("Ogg filesize", len(all_sound))
+        # print("Ogg filesize", len(all_sound))
         duration = len(all_sound) - start_time
         all_sound[start_time:].export(decompressed_wave, format="wav")
-        print("Finished wav audio")
+        # print("Finished wav audio")
 
         filesize = os.path.getsize(decompressed_wave)
         print("Transcribing filesize", filesize)
@@ -68,13 +63,13 @@ async def send_messages(websocket, q):
         segments = translation["segments"]
         # remove segments that are longer/later than the duration of the file
         segments = [segment for segment in segments if segment["end"] <= duration / 1000]
-        print(len(segments), "segments: \n", printSegments(segments))
+        # print(len(segments), "segments: \n", print_segments(segments))
         result = None
         if len(segments) > 1:
             result = " ".join([x["text"] for x in segments[:-1]])
             print("Sending result", result)
             start_time += min(segments[-2]["end"], segments[-1]["start"]) * 1000
-            print("New start time:", start_time)
+            # print("New start time:", start_time)
         if len(segments) > 0:
             response = {
                 "tentative": segments[-1]["text"]
@@ -84,13 +79,19 @@ async def send_messages(websocket, q):
             await websocket.send(json.dumps(response))
 
 
-def printSegments(segments):
+def print_segments(segments):
     return "\n".join(
         [" ".join(["          ", str(x["start"]), str(x["end"]), str(x["text"])]) for x in segments]
     )
 
 
-# Start websocket server on port 8000
+def clean_up_files():
+    if os.path.exists(streamed_audio):
+        os.remove(streamed_audio)
+    if os.path.exists(decompressed_wave):
+        os.remove(decompressed_wave)
+
+
 if __name__ == "__main__":
     start_server = websockets.serve(websocket_handler, "localhost", 8000)
     asyncio.get_event_loop().run_until_complete(start_server)
