09445d67565c3066bd0991cd4e2f8006f21387a6
Mathias Arens <mathias@arens.ma>
2023-07-15 09:54:50 +0200

    Cleanup

--- infrastructure/install.sh
+++ infrastructure/install.sh
@@ -4,8 +4,7 @@ git clone https://github.com/tatellos/whisper-stream.git
 
 #sudo apt update
 #sudo apt upgrade -y
-sudo apt install -y ffmpeg #nginx xorg nvidia-driver-460
-#sudo reboot
+sudo apt install -y ffmpeg #nginx
 
 cd whisper-stream/server
 pip install --upgrade pip
@@ -14,6 +13,8 @@ pip install -r requirements.txt
 #mv large-v2.pt ~/.cache/whisper/
 python3 main.py
 
+# the rest is only for serving the static files from the same server
+sudo apt install -y nginx
 sudo mkdir /etc/nginx/ssl
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
 
